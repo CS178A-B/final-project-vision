b3f190480a2fcad67724b08625635911dacf8145
Sherwin Sahebi <33410214+Sherv24@users.noreply.github.com>
2020-11-07 15:53:05 -0800

    Made new branch and added calendar that can read in JSON files.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import DataSource from './datasource.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Team Vision
-        </p>
-      </header>
-    </div>
-  );
-}
+import { Inject,ScheduleComponent,Day, Week, WorkWeek, Month, Agenda, EventSettingsModel } from '@syncfusion/ej2-react-schedule';
+
+// import { DataManager,WebApiAdaptor } from '@syncfusion/ej2-data';
+
+class App extends React.Component {
+  schData = DataSource.scheduleData;
+
+  render() {
+    return <ScheduleComponent currentView='Month' selectedDate={new Date(2019, 0, 6)}
+    eventSettings={{ dataSource: this.schData }}>
+      <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+      </ScheduleComponent>
+  }
+};
 
 export default App;
