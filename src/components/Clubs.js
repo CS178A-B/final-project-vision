3a42d91a44b0be3aafb0f51a050d54120b227fce
Sherwin Sahebi <33410214+Sherv24@users.noreply.github.com>
2020-11-17 15:59:30 -0800

    Added club components

--- src/components/Clubs.js
+++ src/components/Clubs.js
@@ -1,12 +1,57 @@
 import React from 'react'
 import DataSource from '../mock_data/datasource.json';
 import { Inject,ScheduleComponent,Day, Week, WorkWeek, Month, Agenda, EventSettingsModel } from '@syncfusion/ej2-react-schedule';
-class Clubs extends React.Component { 
+
+import '../styles/Clubs.css'
+
+const dict = {
+    "isChessClub" : "ucrchessclub",
+    "isACM" : "ucracm",
+    "isPersianClub" : "ucrpersianclub"
+}
+
+
+class Clubs extends React.Component {
+    state = {
+        isChessClub : false,
+        isACM : false,
+        isPersianClub : false
+    };
+    
+    handleClick = e =>{
+        this.setState({[e.target.name] : e.target.checked})
+    }
+
+    handleSubmit = e =>{
+        let clubs = []
+        for (let key in this.state){
+            let value = this.state[key]
+            if (value===true) {
+                clubs.push(dict[key])
+            }
+        }
+        console.log(clubs)
+    }
+
     render() {
         return (
-            <div>
-                CLUB LIST HERE
-            </div>
+            <div className="Clubs">
+            <form>
+                <label>
+                Chess Club
+                <input type="checkbox" name="isChessClub" onClick={this.handleClick}/>
+                </label><br />
+                <label>
+                ACM
+                <input type="checkbox" name="isACM" onClick={this.handleClick}/>
+                </label><br />
+                <label>
+                Persian Club
+                <input type="checkbox" name="isPersianClub" onClick={this.handleClick}/>
+                </label>                
+            </form>
+                <button onClick={this.handleSubmit}>Submit</button>
+             </div>
         )
     }
 }
