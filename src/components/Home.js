6b954f18adc448dccff6935539de5c37490798a6
Nicole Nguyen <nicolenguyen@Nicoles-MacBook-Pro.local>
2020-11-18 00:18:15 -0800

    react exploration

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import CircularButton from './CircularButton';
+import LargeButton from './LargeButton/index.js';
 
 class Home extends React.Component { 
-    render() {
-        return <div>LANDING PAGE</div>
-    }
+  render() {
+    return (
+      <div>
+        LANDING PAGE
+        <div>
+          <LargeButton
+              text="TRY VISION FOR FREE"
+              fontColor="white"
+              bgColor="yellow"
+          />
+        </div>
+      </div>
+    );
+  }
 }
+
+
+// example
 
 export default Home;
---
+++ src/components/LargeButton/styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+    color: ${props => props.fontColor || "white"};
+    background-Color: ${props => props.bgColor || "red" };
+    border: 2px black;
+    font-size: 16px;
+    min-height: 15px;
+    min-width: 40px;
+`;
+
+// destructing --> exporting someting only using {}
+export const TopText = styled.h1`
+ font-size: 20px;
+`;
+
