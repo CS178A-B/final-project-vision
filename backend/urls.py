9d2967b870e2f5a4ebbaf6ebc4c6735faebad7e2
Viraj Dhillon <virajdhillon@gmail.com>
2020-11-17 20:16:03 -0800

    Views.py - Able to access MongoDB database and able to read in POST requests to send and receive data to/from the Database/frontend. Code can retrieve information for user from Database as well as add users and add organizations to users. urls.py - Added endpoint api/addOrganization

--- backend/urls.py
+++ backend/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('calendar',views.index),
     path('clubs',views.index),
-    path('api/getCalendarInfo', views.getCalendarInfo)
+    path('api/getCalendarInfo', views.getCalendarInfo),
+    path('api/addOrganization', views.addOrganization)
     #TODO
     # update (PUT)
     # delete (DELETE)
