21cf7bb3b3f14e1dba4c5826e808f56c0242d5c4
vishshukla <vwshukla@gmail.com>
2021-03-04 23:55:11 -0800

    created boilerplaye for joinorg

--- src/components/JoinOrganization.js
+++ src/components/JoinOrganization.js
@@ -1,13 +1,41 @@
 // src/components/JoinOrganization.js
 
-import React from "react";
-import { withAuth0 } from "@auth0/auth0-react";
+import React, { useEffect } from "react";
+import { withAuthenticationRequired, useAuth0 } from "@auth0/auth0-react";
+import Loading from "./loading";
+
+const API = 'https://team-vision-cs178.herokuapp.com/api/'
 
 const JoinOrganization = props => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  useEffect(() => {
+    const joinOrgHandler = async () => {
+      try {
+        const token = await getAccessTokenSilently();
+        const myHeaders = new Headers();
+        const data = new FormData();
+        myHeaders.append('Authorization', `Bearer ${token}`)
+        data.append("organization_id", props.match.params.id)
+        data.append("public_true_or_false",'true')
+        fetch(API + "addOrganization", {
+          method: 'POST',
+          headers: myHeaders,
+          body: data,
+        }).then(res => res.json())
+        .then( res => console.log(res))
+      } catch( error) {
+        console.log(error)
+      }
+    }
+    joinOrgHandler()
+  })
     console.log(props.match.params.id)
     return (
       <div>In join org page -- check console</div>
     );
 }
 
-export default withAuth0(JoinOrganization);
+export default withAuthenticationRequired(JoinOrganization, {
+  onRedirecting : () => <Loading />,
+});
