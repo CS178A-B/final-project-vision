d862a5a07370368cc93bc1f9f4717f00ee9e6b8b
nicohhle <nnguy099@ucr.edu>
2021-03-10 12:55:33 -0800

    styling for OrganizationProfile: name, description, and join link placed

--- src/components/OrganizationProfile.js
+++ src/components/OrganizationProfile.js
@@ -4,6 +4,9 @@ import Loading from './loading'
 import CreateEvent from './CreateEvent'
 import styled from 'styled-components';
 import { bind } from 'jest-each';
+import { Col, Container, Row } from 'react-bootstrap';
+import { bgColor } from './colors';
+import { Block } from './Featureblock/styled';
 
 const Button = styled.button`
   background-color: red;
@@ -47,10 +50,36 @@ const RenderOrganizationInfo = (props) => {
         <>
             {org_id === "" ? <></> :  //only render things when we have an organization info to display
             <>
+                <Row>
+                    <Col md={{ span: 8}} style={{ marginBottom: "20px", marginRight: "0" }}>
+                        <Container style={{borderRadius: "20px", background: bgColor.BlueGray, width: "80%", marginTop: "20px", padding: "40px", alignSelf: "left"}}>
+                        <h4 style={{ textAlign: "center", fontSize:"1.1em" }}>Organization: {org_name}</h4>     
+                        </Container>
+                        <Container style={{borderRadius: "20px", background: bgColor.LightGray, width: "80%", marginTop: "20px", padding: "10px"}}>
+                        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+
+                        
+                        <div style={{ marginTop: "10px", marginBottom: "20px", fontSize: "0.8em"}}>
+                            Org Description: {org_description}
+                        </div>
+                        <div>
+                        <p style={{ fontSize: "0.6em" }}>Join Link: {joinLink}</p>   
+                        </div>
+                        </div>
+                        </Container>
+                    </Col>
+                    <Col md={{span: 4}} style={{ margin: "auto" }}>
+                        {/* <Container style={{ background: bgColor.LightBlue, borderRadius: "20px", height: "80vh"}}>
+
+                        </Container> */}
+                    </Col>
+                </Row>
+                
+                {/* <div>Org Description: {org_description}</div> */}
                 <div>Role: {delegator ? <>Delegator <CreateEvent organization_id={org_id}/></> : "Member"}</div>
-                <div>Org Name: {org_name}</div>
-                <div>Org Description: {org_description}</div>
-                <div>Join Link: {joinLink}</div>
+                {/* <div>Org Name: {org_name}</div> */}
+                {/* <div>Org Description: {org_description}</div> */}
+                {/* <div>Join Link: {joinLink}</div> */}
                 
                 <div>{Object.keys(org_events).map((item,i) => 
                     <div key={i}>
