0bb6588074e40351689adafeb11e7fa33d5d22f1
vishshukla <vwshukla@gmail.com>
2021-03-10 10:45:40 -0800

    attempting to add (join/<hash>) view to render the actual react app

--- backend/urls.py
+++ backend/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('organizations',views.index),
     path('calendar',views.index),
     path('about', views.index),
+    path('join/', views.index),
     path('api/getCalendarInfo', views.getCalendarInfo),
     path('api/addOrganization', views.addOrganization),
     path('api/deleteEvent', views.deleteEvent),
